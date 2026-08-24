826e2e09e1f4fbd880f85619e43a291dcf711a35
Petbd <155575542+Petbd@users.noreply.github.com>
2026-08-24 22:01:34 +0200

    Remove scoped registration for HubUrlProvider

--- DominoOnline.Client/Program.cs
+++ DominoOnline.Client/Program.cs
@@ -9,6 +9,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Регистрируем наш сервис как Singleton (один на всё приложение)
 builder.Services.AddSingleton<GameHubService>();
-builder.Services.AddScoped<HubUrlProvider>();
 
 await builder.Build().RunAsync();
